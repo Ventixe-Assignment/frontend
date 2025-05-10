3c6409930d9234c36e73f0e054da019002091277
Janne <tapioninetytwo@live.se>
2025-05-10 15:40:28 +0200

    working on routing to be able to show something at all

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Events from './assets/pages/Events'
+import Bookings from './assets/pages/Bookings'
+import Dashboard from './assets/pages/Dashboard'
+import PortalLayout from './assets/layouts/PortalLayout'
+import CenterLayout from './assets/layouts/CenterLayout'
 
 function App() {
-
-
   return (
     <Routes>
-      <Route />
+      <Route element={<PortalLayout/>} />
     </Routes>
   )
 }
