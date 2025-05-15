6c7414879033736f5f38eb63f47eacb169986b23
Janne <tapioninetytwo@live.se>
2025-05-15 16:43:05 +0200

    added a lil function for login to go to home

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  return (
+const navigate = useNavigate()
+const handleLogin = (e) => {
+    e.preventDefault()
+    navigate('/home')
+}
+return (
     <div>
         <h1 className='create-account-title'>Login</h1>
-        
+
         <form method="post" novalidate>
 
             <button type="submit" name="provider" value="Google" className="btn btn-external-login">
@@ -33,14 +39,14 @@ const Login = () => {
                 <input type='checkbox' /> <span>Remember me</span>
             </div>
 
-            <button type="submit" className='btn btn-register-login'>Log in</button>
+            <button type="submit" onClick={handleLogin} className='btn btn-register-login'>Log in</button>
         </form>
 
         <div className='account-already'>
             <p>Don't have an account?</p> <a href='/register' >Go to Register</a>
         </div>
     </div>
-  )
+    )
 }
 
 export default Login
