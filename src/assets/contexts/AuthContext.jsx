b9b98ec4eaa3877166bec6b450eddede66c3765c
Janne <tapioninetytwo@live.se>
2025-05-29 20:55:14 +0200

    entered a parameter formData into the register function

--- src/assets/contexts/AuthContext.jsx
+++ src/assets/contexts/AuthContext.jsx
@@ -80,13 +80,13 @@ const AuthProvider = ({children}) => {
         }
     }
 
-    const postRegister = async () => {
+    const postRegister = async (formData) => {
 
         try {
             const res = await fetch(`${apiConnection}/register`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(registerFormData)
+                body: JSON.stringify(formData)
             })
 
             if(!res.ok) {
