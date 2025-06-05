0bf083ad5657aded1c6aace7dbfabcda09245e48
Janne <tapioninetytwo@live.se>
2025-06-05 14:16:20 +0200

    changed the height for centerlayout to be min-height since terms got weird with set height

--- src/assets/pages/Terms.jsx
+++ src/assets/pages/Terms.jsx
@@ -6,29 +6,28 @@ const Terms = () => {
         <h1 className='create-account-title'>Terms & Conditions</h1>
         <div>
             <div className='divider'></div>
-            <div className='title-regular-14 mb-1'>  Welcome to Ventixe! These Terms and Conditions govern your use of our website and services, including but not limited to event listings, ticket bookings, and any other features we may offer. By accessing or using our services, you agree to comply with and be bound by these Terms. If you do not agree with any part of these Terms, you must not use our services.</div>
+            <div className='title-regular-12 mb-1'>  Welcome to Ventixe! These Terms and Conditions govern your use of our website and services, including but not limited to event listings, ticket bookings, and any other features we may offer. By accessing or using our services, you agree to comply with and be bound by these Terms. If you do not agree with any part of these Terms, you must not use our services.</div>
           
-            <div className='title-bold-18 mb-05'>1. Acceptance of Terms</div>
-            <p className='title-regular-12 mb-05'>By using Ventixe, you confirm that you are at least 18 years old or have the consent of a parent or guardian. You agree to provide accurate and complete information when creating an account or making a booking.</p>
+            <h5>1. Acceptance of Terms</h5>
+            <p className='title-regular-14 mb-1'>By using Ventixe, you confirm that you are at least 18 years old or have the consent of a parent or guardian. You agree to provide accurate and complete information when creating an account or making a booking.</p>
   
-            <div className='title-bold-18 mb-05'>2. Services Offered</div>
-            <p className='title-regular-12 mb-05'>Ventixe provides a platform for users to discover and book various events, including music concerts, hockey games, dance performances, and more. We may also implement a booking system for users to purchase tickets directly through our platform.</p>
-   
-                    
-            <div className='title-bold-18 mb-05'>3. User Accounts</div>
-            <p className='title-regular-12 mb-05'>To access certain features of our services, you may need to create an account. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account.</p>
+            <h5>2. Services Offered</h5>
+            <p className='title-regular-14 mb-1'>Ventixe provides a platform for users to discover and book various events, including music concerts, hockey games, dance performances, and more. We may also implement a booking system for users to purchase tickets directly through our platform.</p>
+             
+            <h5>3. User Accounts</h5>
+            <p className='title-regular-14 mb-1'>To access certain features of our services, you may need to create an account. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account.</p>
             
-            <div className='title-bold-18 mb-05'>4. Event Listings and Bookings</div>
-            <p className='title-regular-12 mb-05'>Ventixe strives to provide accurate and up-to-date information regarding events. However, we do not guarantee the accuracy, completeness, or reliability of event details. All bookings are subject to availability, and we reserve the right to cancel or modify events as necessary.</p>
+            <h5>4. Event Listings and Bookings</h5>
+            <p className='title-regular-14 mb-1'>Ventixe strives to provide accurate and up-to-date information regarding events. However, we do not guarantee the accuracy, completeness, or reliability of event details. All bookings are subject to availability, and we reserve the right to cancel or modify events as necessary.</p>
             
-            <div className='title-bold-18 mb-05'>5. Payment and Fees</div>
-            <p className='title-regular-12 mb-05'>When making a booking, you agree to pay all applicable fees and charges. All payments are processed securely, and you are responsible for any additional fees that may apply, such as service charges or taxes.</p>
+            <h5>5. Payment and Fees</h5>
+            <p className='title-regular-14 mb-1'>When making a booking, you agree to pay all applicable fees and charges. All payments are processed securely, and you are responsible for any additional fees that may apply, such as service charges or taxes.</p>
             
-            <div className='title-bold-18 mb-05'>6. Cancellation and Refund Policy</div>
-            <p className='title-regular-12 mb-05'>Please review our cancellation and refund policy carefully before making a booking. Specific terms may vary by event, and we will provide clear information regarding cancellations and refunds at the time of booking.</p>
+            <h5>6. Cancellation and Refund Policy</h5>
+            <p className='title-regular-14 mb-1'>Please review our cancellation and refund policy carefully before making a booking. Specific terms may vary by event, and we will provide clear information regarding cancellations and refunds at the time of booking.</p>
             
-            <div className='title-bold-18 mb-05'>7. Intellectual Property</div>
-            <p className='title-regular-12 mb-05'>All content on Ventixe, including text, graphics, logos, and software, is the property of Ventixe or its licensors and is protected by copyright and other intellectual property laws. You may not use, reproduce, or distribute any content without our prior written consent.</p>
+            <h5>7. Intellectual Property</h5>
+            <p className='title-regular-14 mb-1'>All content on Ventixe, including text, graphics, logos, and software, is the property of Ventixe or its licensors and is protected by copyright and other intellectual property laws. You may not use, reproduce, or distribute any content without our prior written consent.</p>
 
         </div>
     </div>
